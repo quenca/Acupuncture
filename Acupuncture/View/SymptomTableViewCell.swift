a7a583e4a9a76214e2a9bfeeaee54a1ebbc03b77
Gustavo Quenca <gustavo.quenca@icloud.com>
2019-04-16 18:56:27 -0300

    Implement CoreData and Save Symptoms

---
+++ Acupuncture/View/SymptomTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  SymptomTableViewCell.swift
+//  Acupuncture
+//
+//  Created by Gustavo Quenca on 08/04/19.
+//  Copyright © 2019 Quenca. All rights reserved.
+//
+
+import Foundation
---
+++ Acupuncture/View/SymptomViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SymptomViewController.swift
+//  Acupuncture
+//
+//  Created by Gustavo Quenca on 08/04/19.
+//  Copyright © 2019 Quenca. All rights reserved.
+//
+
+import Foundation
